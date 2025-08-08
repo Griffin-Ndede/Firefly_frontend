476ce09032541314397c2fb7b9eb4e7bdbb0efc7
Griffin <omondigriffin4@gmail.com>
2025-08-08 16:06:17 +0300

    Style: Styling changes to the cards in section 2 of the homepage

--- src/Pages/Home.jsx
+++ src/Pages/Home.jsx
@@ -21,8 +21,8 @@ function Home() {
             </section>
             <section className='md:h-fit bg-custom-beige py-10'>
                 <h1 className='text-5xl font-bold text-center mb-10'>Our <span className='text-custom-yellow'>solution</span></h1>
-                <div className='grid grid-cols-1 md:grid-cols-2 lg:grid-cols-4 gap-10 px-10 mx-auto'>
-                    <div className="shadow-2xl p-6 md:p-8 w-auto bg-custom-yellow rounded-3xl space-y-6">
+                <div className='grid grid-cols-1 md:grid-cols-2 lg:grid-cols-4 gap-16 px-10 mx-auto'>
+                    <div className="shadow-2xl p-6 md:p-8 w-auto bg-custom-yellow rounded-3xl space-y-6 transition-transform transform hover:scale-[1.05] hover:cursor-pointer">
                         {/* Icons Row */}
                         <div className="flex items-center justify-between relative">
                             <Grip strokeWidth={0.5} className="text-white w-16 h-16 md:w-16 md:h-16" />
@@ -30,40 +30,49 @@ function Home() {
                         </div>
 
                         {/* Text Content */}
-                        <div>
-                            <h1 className="text-2xl md:text-3xl font-bold text-gray-900 mb-2">
-                                What <br /> we offer
-                            </h1>
-                            <p className="text-lg font-light leading-relaxed text-gray-800">
-                                Firefly empowers cultural preservation, nurtures creative talent, and reshapes African narratives through education, mentorship, and authentic storytelling.
-                            </p>
-                        </div>
+                        <h1 className="text-2xl md:text-3xl font-bold  mb-2">
+                            What <br /> we offer
+                        </h1>
+                        <p className="text-base font-light leading-relaxed text-custom-gray">
+                            Firefly empowers cultural preservation, nurtures creative talent, and reshapes African narratives through education, mentorship, and authentic storytelling.
+                        </p>
                     </div>
 
-
-                    <div className='shadow-2xl p-6 md:p-8 w-auto bg-custom-yellow rounded-3xl space-y-6'>
-                        <div className="flex items-center justify-between relative">
+                    <div className='shadow-2xl p-6 md:p-8 w-auto bg-custom-yellow rounded-3xl space-y-6 transition-transform transform hover:scale-[1.05] hover:cursor-pointer'>
+                        <div className="flex items-center justify-between relative hover:tranform">
                             <Ratio strokeWidth={0.5} className="text-white w-16 h-16 md:w-16 md:h-16" />
                             <LibraryBig strokeWidth={0.5} className="text-white w-32 h-32 md:w-28 md:h-28  lg:h-32 lg:w-32 absolute top-0 right-2" />
                         </div>
-                        <h1 className='text-2xl md:text-3xl font-bold text-gray-900 mb-2'>Cultural <br />education</h1>
-                        <p className='text-lg font-light leading-relaxed text-gray-800'>Engaging, culturally immersive programs that teach children vernacular languages and African heritage through creative visual arts.</p>
+                        <h1 className='text-2xl md:text-3xl font-bold  mb-2'>
+                            Cultural <br />education
+                        </h1>
+                        <p className='text-base font-light leading-relaxed text-custom-gray'>
+                            Engaging, culturally immersive programs that teach children vernacular languages and African heritage through creative visual arts.
+                        </p>
                     </div>
-                    <div className='shadow-2xl p-6 md:p-8 w-auto bg-custom-yellow rounded-3xl space-y-6'>
+                    <div className='shadow-2xl p-6 md:p-8 w-auto bg-custom-yellow rounded-3xl space-y-6 transition-transform transform hover:scale-[1.05] hover:cursor-pointer'>
                         <div className="flex items-center justify-between relative">
                             <Blend strokeWidth={0.5} className="text-white w-16 h-16 md:w-16 md:h-16" />
                             <Palette strokeWidth={0.5} className="text-white w-32 h-32 md:w-28 md:h-28 lg:h-32 lg:w-32 absolute top-0 right-2" />
                         </div>
-                        <h1 className='text-2xl md:text-3xl font-bold text-gray-900 mb-2'>Artistic <br /> empowerment</h1>
-                        <p className='text-lg font-light leading-relaxed text-gray-800'>Empowering apsiring visual artists from diverse backgrounds with mentorship, skills training, and pathways to sustainable careers</p>
+                        <h1 className='text-2xl md:text-3xl font-bold  mb-2'>
+                            Artistic <br /> empowerment
+                        </h1>
+                        <p className='text-base font-light leading-relaxed text-custom-gray'>
+                            Empowering apsiring visual artists from diverse backgrounds with mentorship, skills training, and pathways to sustainable careers
+                        </p>
                     </div>
-                    <div className='shadow-2xl p-6 md:p-8 w-auto bg-custom-yellow rounded-3xl space-y-6'>
+                    <div className='shadow-2xl p-6 md:p-8 w-auto bg-custom-yellow rounded-3xl space-y-6 transition-transform transform hover:scale-[1.05] hover:cursor-pointer'>
                         <div className="flex items-center justify-between relative">
                             <SquareStack strokeWidth={0.5} className="text-white w-16 h-16 md:w-16 md:h-16" />
                             <ScrollText strokeWidth={0.5} className="text-white w-32 h-32 md:w-28 md:h-28 lg:h-32 lg:w-32 absolute top-0 right-2" />
                         </div>
-                        <h1 className='text-2xl md:text-3xl font-bold text-gray-900 mb-2'>Authentic <br />Storytelling</h1>
-                        <p className='text-lg font-light leading-relaxed text-gray-800'>Producing orignal animated films and media content that genuinely reflex African experiences and challenge stereotypes</p>
+                        <h1 className='text-2xl md:text-3xl font-bold  mb-2'>
+                            Authentic <br />Storytelling
+                        </h1>
+                        <p className='text-base font-light leading-relaxed text-custom-gray'>
+                            Producing original animated films and media content that genuinely reflex African experiences and challenge stereotypes
+                        </p>
                     </div>
                 </div>
             </section>
@@ -75,8 +84,8 @@ function Home() {
                             src="/images/Firefly_Mzizi.jpg"
                             alt="Firefly_mizizi"
                             className='h-64 w-full object-cover rounded-t-3xl' />
-                        <h1 className='text-2xl md:text-3xl font-bold text-gray-900 px-6 py-4'>Firefly MIZIZI</h1>
-                        <p className='text-lg font-light leading-relaxed text-gray-800 px-6 pb-6'>Cultural preservation and vernacular language education through visual arts</p>
+                        <h1 className='text-2xl md:text-3xl font-bold  px-6 py-4'>Firefly MIZIZI</h1>
+                        <p className='text-lg font-light leading-relaxed text-custom-gray px-6 pb-6'>Cultural preservation and vernacular language education through visual arts</p>
                     </div>
 
                     <div className='bg-custom-beige rounded-3xl'>
@@ -84,8 +93,8 @@ function Home() {
                             src="/images/Firefly_Talanta.jpg"
                             alt="Firefly_talanta"
                             className='h-64 w-full object-cover rounded-t-3xl' />
-                        <h1 className='text-2xl md:text-3xl font-bold text-gray-900 px-6 py-4'>Firefly TALANTA</h1>
-                        <p className='text-lg font-light leading-relaxed text-gray-800 px-6 pb-6'>Fellowship and mentorship programs for youth in visual arts</p>
+                        <h1 className='text-2xl md:text-3xl font-bold  px-6 py-4'>Firefly TALANTA</h1>
+                        <p className='text-lg font-light leading-relaxed text-custom-gray px-6 pb-6'>Fellowship and mentorship programs for youth in visual arts</p>
                     </div>
 
                     <div className='bg-custom-beige rounded-3xl'>
@@ -93,8 +102,8 @@ function Home() {
                             src="/images/Firefly_Taswira.png"
                             alt="Firefly_taswira"
                             className='h-64 w-full object-cover rounded-t-3xl' />
-                        <h1 className='text-2xl md:text-3xl font-bold text-gray-900 px-6 py-4'>Firefly TASWIRA</h1>
-                        <p className='text-lg font-light leading-relaxed text-gray-800 px-6 pb-6'>Production of animated content that showcases authentic African stories.</p>
+                        <h1 className='text-2xl md:text-3xl font-bold  px-6 py-4'>Firefly TASWIRA</h1>
+                        <p className='text-lg font-light leading-relaxed text-custom-gray px-6 pb-6'>Production of animated content that showcases authentic African stories.</p>
                     </div>
                 </div>
             </section>
