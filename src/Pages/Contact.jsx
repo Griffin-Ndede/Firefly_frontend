8ddea27064fd252f7b44d0d047b12be4f93a186a
Griffin <omondigriffin4@gmail.com>
2025-08-12 23:31:06 +0300

    Feat: adds content to the contact us page

--- src/Pages/Contact.jsx
+++ src/Pages/Contact.jsx
@@ -1,10 +1,19 @@
 import React from 'react'
+import Navbar from '../Components/Navbar'
 
 function Contact() {
   return (
-    <div>
-      This is my contact page
-    </div>
+    <>
+    <Navbar/>
+    <section className='mt-24 grid grid-cols-2'>
+      <div className='h-fit w-2/3 rounded-3xl bg-custom-beige mx-10 py-6 px-8'>
+<h1>Get in touch</h1>
+<p>Let&apos;s chat, Reach out to us!</p>
+<p>Norem ipsum dolor sit amet, consectetur adipiscing elit. Nunc vulputate libero et velit interdum, ac aliquet odio mattis.</p>
+
+      </div>
+    </section>
+    </>
   )
 }
 
