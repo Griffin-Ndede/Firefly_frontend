b24ceb852ca482a12b0b68048402a8afe06f18bb
Griffin <omondigriffin4@gmail.com>
2025-08-18 22:26:58 +0300

    Refactor: creates an array where data can be fetched from in the home page for the card components

--- src/Pages/Home.jsx
+++ src/Pages/Home.jsx
@@ -5,6 +5,46 @@ import FloatingIconsBackground from '../Components/FloatingIconsBackground'
 import { CircleCheckBig, Grip, ScrollText, Users, SquareStack, Palette, Brush, Blend, Ratio, Library, LibraryBig } from 'lucide-react'
 
 function Home() {
+
+    const cardData = [
+        {
+            title: <>What <br /> we offer</>,
+            description:
+                "Firefly empowers cultural preservation, nurtures creative talent, and reshapes African narratives through education, mentorship, and authentic storytelling.",
+            icons: {
+                main: Grip,
+                background: CircleCheckBig,
+            },
+        },
+        {
+            title: <>Cultural <br /> education</>,
+            description:
+                "Engaging, culturally immersive programs that teach children vernacular languages and African heritage through creative visual arts.",
+            icons: {
+                main: Ratio,
+                background: LibraryBig,
+            },
+        },
+        {
+            title: <>Artistic <br /> empowerment</>,
+            description:
+                "Empowering aspiring visual artists from diverse backgrounds with mentorship, skills training, and pathways to sustainable careers.",
+            icons: {
+                main: Blend,
+                background: Palette,
+            },
+        },
+        {
+            title: <>Authentic <br />Storytelling</>,
+            description:
+                "Producing original animated films and media content that genuinely reflect African experiences and challenge stereotypes.",
+            icons: {
+                main: SquareStack,
+                background: ScrollText,
+            },
+        },
+
+    ]
     return (
         <>
             <Navbar />
@@ -24,58 +64,21 @@ function Home() {
             <section className='md:h-fit bg-custom-beige py-10'>
                 <h1 className='text-5xl font-bold text-center mb-10'>Our <span className='text-custom-yellow'>solution</span></h1>
                 <div className='grid grid-cols-1 md:grid-cols-2 lg:grid-cols-4 gap-16 px-10 mx-auto'>
-                    <div className="shadow-2xl p-6 md:p-8 w-auto bg-custom-yellow rounded-3xl space-y-6 transition-transform transform hover:scale-[1.05] hover:cursor-pointer">
-                        {/* Icons Row */}
-                        <div className="flex items-center justify-between relative">
-                            <Grip strokeWidth={0.5} className="text-white w-16 h-16 md:w-16 md:h-16" />
-                            <CircleCheckBig strokeWidth={0.5} className="text-white w-32 h-32 md:w-28 md:h-28 lg:h-32 lg:w-32 absolute top-0 right-2" />
+                    {cardData.map((card, index) => (
+                        <div key={index} className="shadow-2xl p-6 md:p-8 w-auto bg-custom-yellow rounded-3xl space-y-6 transition-transform transform hover:scale-[1.05] hover:cursor-pointer">
+                            <div
+                                className="flex items-center justify-between relative lg:mb-12 hover:transform">
+                                <card.icons.main strokeWidth={0.5} className="text-white w-16 h-16 md:w-16 md:h-16" />
+                                <card.icons.background strokeWidth={0.5} className="text-white w-32 h-32 md:w-28 md:h-28 lg:h-32 lg:w-32 absolute top-0 right-2" />
+                            </div>
+                            <h1 className="text-2xl md:text-3xl font-bold  mb-2">
+                                {card.title}
+                            </h1>
+                            <p className="text-base font-light leading-relaxed text-custom-gray">
+                                {card.description}
+                            </p>
                         </div>
-
-                        {/* Text Content */}
-                        <h1 className="text-2xl md:text-3xl font-bold  mb-2">
-                            What <br /> we offer
-                        </h1>
-                        <p className="text-base font-light leading-relaxed text-custom-gray">
-                            Firefly empowers cultural preservation, nurtures creative talent, and reshapes African narratives through education, mentorship, and authentic storytelling.
-                        </p>
-                    </div>
-
-                    <div className='shadow-2xl p-6 md:p-8 w-auto bg-custom-yellow rounded-3xl space-y-6 transition-transform transform hover:scale-[1.05] hover:cursor-pointer'>
-                        <div className="flex items-center justify-between relative hover:tranform">
-                            <Ratio strokeWidth={0.5} className="text-white w-16 h-16 md:w-16 md:h-16" />
-                            <LibraryBig strokeWidth={0.5} className="text-white w-32 h-32 md:w-28 md:h-28  lg:h-32 lg:w-32 absolute top-0 right-2" />
-                        </div>
-                        <h1 className='text-2xl md:text-3xl font-bold  mb-2'>
-                            Cultural <br />education
-                        </h1>
-                        <p className='text-base font-light leading-relaxed text-custom-gray'>
-                            Engaging, culturally immersive programs that teach children vernacular languages and African heritage through creative visual arts.
-                        </p>
-                    </div>
-                    <div className='shadow-2xl p-6 md:p-8 w-auto bg-custom-yellow rounded-3xl space-y-6 transition-transform transform hover:scale-[1.05] hover:cursor-pointer'>
-                        <div className="flex items-center justify-between relative">
-                            <Blend strokeWidth={0.5} className="text-white w-16 h-16 md:w-16 md:h-16" />
-                            <Palette strokeWidth={0.5} className="text-white w-32 h-32 md:w-28 md:h-28 lg:h-32 lg:w-32 absolute top-0 right-2" />
-                        </div>
-                        <h1 className='text-2xl md:text-3xl font-bold  mb-2'>
-                            Artistic <br /> empowerment
-                        </h1>
-                        <p className='text-base font-light leading-relaxed text-custom-gray'>
-                            Empowering apsiring visual artists from diverse backgrounds with mentorship, skills training, and pathways to sustainable careers
-                        </p>
-                    </div>
-                    <div className='shadow-2xl p-6 md:p-8 w-auto bg-custom-yellow rounded-3xl space-y-6 transition-transform transform hover:scale-[1.05] hover:cursor-pointer'>
-                        <div className="flex items-center justify-between relative">
-                            <SquareStack strokeWidth={0.5} className="text-white w-16 h-16 md:w-16 md:h-16" />
-                            <ScrollText strokeWidth={0.5} className="text-white w-32 h-32 md:w-28 md:h-28 lg:h-32 lg:w-32 absolute top-0 right-2" />
-                        </div>
-                        <h1 className='text-2xl md:text-3xl font-bold  mb-2'>
-                            Authentic <br />Storytelling
-                        </h1>
-                        <p className='text-base font-light leading-relaxed text-custom-gray'>
-                            Producing original animated films and media content that genuinely reflex African experiences and challenge stereotypes
-                        </p>
-                    </div>
+                    ))}
                 </div>
             </section>
             <section className='h-fit mb-10'>
@@ -110,8 +113,8 @@ function Home() {
                 </div>
             </section>
             <section id='team'
-            className='h-screen py-4 relative'>
-                      <FloatingIconsBackground />
+                className='h-screen py-4 relative'>
+                <FloatingIconsBackground />
 
                 <h1 className='text-center text-5xl font-bold text-custom-yellow my-3'>Meet our team</h1>
                 <p className='font-light text-custom-gray mt-4 text-xl text-center my-6'>The professional team behind firefly</p>
@@ -122,7 +125,7 @@ function Home() {
                         <h1 className='border-b-2 border-custom-yellow  w-fit mx-auto pb-2 text-2xl'>Sheryl Kambuni</h1>
                         <p className='font-light text-custom-gray mt-4 text-xl'>CEO & Visionary Leader</p>
                     </div>
-                   <div className='text-center mx-auto'>
+                    <div className='text-center mx-auto'>
                         <img src="https://media.istockphoto.com/id/1299077558/photo/lead-yourself-to-a-life-of-success.jpg?s=612x612&w=0&k=20&c=OQZPSnM1Eq-4Xx8bxJE8KQ5olJFfRw_YMc29aQ0Au6U=" alt="CEO"
                             className='h-56 w-56 object-cover mb-4 rounded-full p-2 mx-auto' />
                         <h1 className='border-b-2 border-custom-yellow  w-fit mx-auto pb-2 text-2xl'>Jean Kambuni</h1>
