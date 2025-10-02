2f548888d3e46af91c5c7528288d72e2f6027b9f
Griffin <omondigriffin4@gmail.com>
2025-10-02 15:42:08 +0300

    Feat: changes the scroll bars colors to the company colors

--- src/Components/Navbar.jsx
+++ src/Components/Navbar.jsx
@@ -1,6 +1,7 @@
 import React, { useState } from 'react'
 import { Menu, X } from 'lucide-react'
 import { Link } from 'react-router'
+import { motion } from "motion/react"
 
 function Navbar() {
   const [isOpen, setIsOpen] = useState(false)
@@ -28,9 +29,8 @@ function Navbar() {
 
         {/* Nav Links */}
         <nav
-          className={`${
-            isOpen ? 'flex items-end w-fit float-end' : 'hidden'
-          } absolute top-full right-0 w-fit  bg-white  md:static md:flex md:w-auto md:bg-transparent md:items-center gap-3 md:gap-8  md:mt-0 px-6 py-4 md:p-0 flex-col md:flex-row  md:rounded-none md:shadow-none rounded-3xl`}
+          className={`${isOpen ? 'flex items-end w-fit float-end' : 'hidden'
+            } absolute top-full right-0 w-fit  bg-white  md:static md:flex md:w-auto md:bg-transparent md:items-center gap-3 md:gap-8  md:mt-0 px-6 py-4 md:p-0 flex-col md:flex-row  md:rounded-none md:shadow-none rounded-3xl`}
         >
           <Link
             to="/"
@@ -38,18 +38,26 @@ function Navbar() {
           >
             Home
           </Link>
-          <a
+          <motion.a
+            initial={{ y: 20, opacity: 0, scaleY: 0.8 }}
+            animate={{ y: 0, opacity: 1, scaleY: 1 }}
+            whileHover={{ y: -5 }}
+            transition={{
+              duration: 0.6,
+              ease: [0.25, 0.1, 0.25, 1], // smooth ease like iOS
+            }}
             href="#services"
-            className="py-2 px-5 rounded-3xl  transition-all duration-200 text-gray-800 hover:bg-custom-yellow  "
+            className="py-2 px-5 rounded-3xl transition-all duration-1000 text-gray-800 hover:bg-custom-yellow"
           >
             Our services
-          </a>
+          </motion.a>
+
           <a href='#about'
             className="py-2 px-5 rounded-3xl  transition-all duration-200 text-gray-800 hover:bg-custom-yellow  "
           >
             About Us
           </a>
-          
+
           <a
             href="#contact"
             className="py-2 px-5 rounded-3xl  transition-all duration-200 text-gray-800 hover:bg-custom-yellow  "
